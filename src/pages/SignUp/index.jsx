7cbf728e367ec97dcfb609b0c98d5aac209c6e67
stéfany <stelar.dev@gmail.com>
2022-10-08 11:59:05 -0300

    axios installed, user register ok

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -2,11 +2,37 @@ import { Container, Form, Background } from "./styles";
 
 import { FiMail, FiUser, FiLock, FiArrowLeft } from 'react-icons/fi';
 
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { api } from '../../services/api';
+
 import { Input } from '../../components/Input';
 import { TextButton } from "../../components/TextButton";
 import { Button } from "../../components/Button";
 
 export function SignUp() {
+
+  const navigate = useNavigate();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSignUp({ name, email, password }) {
+    try {
+      await api.post("/users", { name, email, password });
+      alert("Usuário cadastrado com sucesso!");
+      navigate("/");
+    } catch (error) {
+      if (error.response) {
+        return alert(error.response.data.message);
+      }
+
+      return alert("Não foi possível completar o cadastro. Tente novamente.");
+    }
+  }
+
   return (
     <Container>
       <Form>
@@ -15,16 +41,23 @@ export function SignUp() {
         <h3>Crie sua conta</h3>
         <Input
           icon={FiUser}
-          placeholder="Nome" />
+          placeholder="Nome"
+          onChange={e => setName(e.target.value)}
+        />
         <Input
           icon={FiMail}
-          placeholder="Email" />
+          placeholder="Email"
+          onChange={e => setEmail(e.target.value)}
+        />
         <Input
           icon={FiLock}
           placeholder="Password"
           type="password"
+          onChange={e => setPassword(e.target.value)}
         />
-        <Button title="Cadastrar" />
+        <Button
+          title="Cadastrar"
+          onClick={() => handleSignUp({ name, email, password })} />
         <TextButton
           icon={FiArrowLeft}
           title="Voltar para o login"
