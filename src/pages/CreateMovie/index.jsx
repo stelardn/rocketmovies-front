f25121db5e74f1385c1bea911f1a63fc163c1969
stéfany <stelar.dev@gmail.com>
2022-10-10 18:46:22 -0300

    create note ok

--- src/pages/CreateMovie/index.jsx
+++ src/pages/CreateMovie/index.jsx
@@ -1,6 +1,7 @@
 import { Container } from "./styles";
 
 import { useState, useEffect } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 
 import { FiArrowLeft } from "react-icons/fi";
 
@@ -12,9 +13,17 @@ import { Button } from "../../components/Button";
 import { TagItem } from "../../components/TagItem";
 import { Tag } from "../../components/Tag";
 
+import { api } from "../../services/api";
+
 export function CreateMovie() {
+  const navigate = useNavigate();
+
   const [tags, setTags] = useState([]);
   const [newTag, setNewTag] = useState("");
+
+  const [title, setTitle] = useState("");
+  const [rating, setRating] = useState(5);
+  const [description, setDescription] = useState("");
 
   function handleAddTag(tag) {
     setTags(previousTags => [...previousTags, tag]);
@@ -24,6 +33,28 @@ export function CreateMovie() {
   function handleRemoveTag(removedTag) {
     const filteredTags = tags.filter(tag => tag !== removedTag);
     setTags(filteredTags);
+  }
+
+  async function addNote() {
+    try {
+      const movieNote = {
+        title,
+        rating,
+        description,
+        tags
+      }
+
+      const response = await api.post("/movie_notes", movieNote);
+      navigate("/");
+
+      alert("Nota criada com sucesso!");
+    } catch (error) {
+      if (error.response) {
+        return alert(error.response.data.message);
+      } else {
+        return alert("Não foi possível adicionar a nota. Tente novamente.")
+      }
+    }
   }
 
   return (
@@ -38,10 +69,20 @@ export function CreateMovie() {
         <h2>Novo filme</h2>
         <form>
           <div className="first-line">
-            <Input placeholder="Título" />
-            <Input placeholder="Sua nota (de 0 a 5)" />
+            <Input
+              placeholder="Título"
+              onChange={e => setTitle(e.target.value)}
+            />
+            <Input
+              placeholder="Sua nota (de 0 a 5)"
+              onChange={e => setRating(Number(e.target.value))}
+            />
           </div>
-          <TextArea placeholder="Observações" rows={6} />
+          <TextArea
+            placeholder="Observações"
+            rows={6}
+            onChange={e => setDescription(e.target.value)}
+          />
         </form>
         <footer>
           <h3>Marcadores</h3>
@@ -65,8 +106,13 @@ export function CreateMovie() {
             />
           </div>
           <div className="buttons">
-            <Button title="Excluir filme" />
-            <Button title="Salvar alterações" />
+            <Button
+              title="Excluir filme"
+            />
+            <Button
+              title="Salvar alterações"
+              onClick={addNote}
+            />
           </div>
         </footer>
       </main>
