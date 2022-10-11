9f2fe4d783fb41f3c69d97c1bc2f138f7421a4c5
stéfany <stelar.dev@gmail.com>
2022-10-11 14:01:31 -0300

    note details ok

--- src/pages/MoviePreview/index.jsx
+++ src/pages/MoviePreview/index.jsx
@@ -7,36 +7,72 @@ import { Tag } from "../../components/Tag";
 
 
 import { FiArrowLeft, FiClock } from "react-icons/fi";
+import { useParams } from "react-router-dom";
+import { api } from "../../services/api";
+import { useEffect, useState } from "react";
+import { authUse } from "../../hooks/auth";
 
 export function MoviePreview() {
+
+  const params = useParams();
+  const [details, setDetails] = useState();
+
+  const { user } = authUse();
+
+  const [avatarURL, setAvatarURL] = useState(`${api.defaults.baseURL}/files/${user.avatar}`);
+
+  useEffect(() => {
+    async function fetchNoteDetails() {
+      const response = await api.get(`/movie_notes/${params.id}`);
+      console.log(response.data);
+      setDetails(response.data);
+    }
+
+    fetchNoteDetails();
+  }, [])
+
   return (
     <Container>
       <Header />
-      <main>
-        <TextButton icon={FiArrowLeft} title="Voltar" to='/' />
-        <div className="title">
-          <h1>Interestelar</h1>
-          <Rating rating={4} />
-        </div>
-        <div className="info">
-          <img src="https://github.com/stelardn.png" />
-          <p>Por Stéfany Larissa</p>
-          <FiClock />
-          <p>19/09/2022 às 22:15</p>
-        </div>
-        <div className="tags">
-          <Tag title="Ficção científica" />
-          <Tag title="Drama" />
-          <Tag title="Família" />
-        </div>
-        <div className="description">
-          <p>
-            Pragas nas colheitas fizeram a civilização humana regredir para uma sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de Cooper, acredita que seu quarto está assombrado por um fantasma que tenta se comunicar com ela. Pai e filha descobrem que o "fantasma" é uma inteligência desconhecida que está enviando mensagens codificadas através de radiação gravitacional, deixando coordenadas em binário que os levam até uma instalação secreta da NASA liderada pelo professor John Brand. O cientista revela que um buraco de minhoca foi aberto perto de Saturno e que ele leva a planetas que podem oferecer condições de sobrevivência para a espécie humana. As "missões Lázaro" enviadas anos antes identificaram três planetas potencialmente habitáveis orbitando o buraco negro Gargântua: Miller, Edmunds e Mann – nomeados em homenagem aos astronautas que os pesquisaram. Brand recruta Cooper para pilotar a nave espacial Endurance e recuperar os dados dos astronautas; se um dos planetas se mostrar habitável, a humanidade irá seguir para ele na instalação da NASA, que é na realidade uma enorme estação espacial. A partida de Cooper devasta Murphy.<br /><br />
-
-            Além de Cooper, a tripulação da Endurance é formada pela bióloga Amelia, filha de Brand; o cientista Romilly, o físico planetário Doyle, além dos robôs TARS e CASE. Eles entram no buraco de minhoca e se dirigem a Miller, porém descobrem que o planeta possui enorme dilatação gravitacional temporal por estar tão perto de Gargântua: cada hora na superfície equivale a sete anos na Terra. Eles entram em Miller e descobrem que é inóspito já que é coberto por um oceano raso e agitado por ondas enormes. Uma onda atinge a tripulação enquanto Amelia tenta recuperar os dados de Miller, matando Doyle e atrasando a partida. Ao voltarem para a Endurance, Cooper e Amelia descobrem que 23 anos se passaram.
-          </p>
-        </div>
-      </main>
+      {
+        details &&
+        <main>
+          <TextButton icon={FiArrowLeft} title="Voltar" to='/' />
+          <div className="title">
+            <h1>
+              {details.title}
+            </h1>
+            <Rating
+              rating={details.rating}
+            />
+          </div>
+          <div className="info">
+            <img src={avatarURL} />
+            <p>
+              {user.name}
+            </p>
+            <FiClock />
+            <p>
+              {details.updated_at}
+            </p>
+          </div>
+          <div className="tags">
+            {
+              details.tags.map((tag, index) => (
+                <Tag
+                  title={tag}
+                  key={String(index)}
+                />
+              ))
+            }
+          </div>
+          <div className="description">
+            <p>
+              {details.description}
+            </p>
+          </div>
+        </main>
+      }
 
     </Container>
   )
