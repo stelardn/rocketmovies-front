e0cf00220eba792b83eb35c9becfe0de9f26e54b
stéfany <stelar.dev@gmail.com>
2022-10-13 10:50:15 -0300

    text button position correction

--- src/components/TextButton/styles.js
+++ src/components/TextButton/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 import { Link } from "react-router-dom";
 
-export const Container = styled(Link)`
+export const Container = styled.button`
   background: none;
   text-decoration: none;
 
